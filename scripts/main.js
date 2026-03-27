e3413bcb3b8d4236ff8eec0e90d5485854ae03d9
helgagaona <gao20003@byui.edu>
2026-03-26 19:50:29 -0600

    week 10 helga

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,6 @@ import './index.js';
 import './mymeals.js'
 import './loader.js'
 import { initHomeGrid } from './index.js'; 
-
+import './modal.js'
 
 initHomeGrid();
---
+++ scripts/modal.js
@@ -0,0 +1,12 @@
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal");
+
+closeBtn.addEventListener("click", function() {
+    modal.classList.add("hide");
+});
+
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        modal.classList.remove("hide"); // show modal
+    }, 6000); // 6000 ms = 6 seconds
+});
